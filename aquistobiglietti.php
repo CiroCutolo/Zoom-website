bb0d973c511f78a8526a872f76646441a1637820
Gaetano Frasca <gaetanofra02@gmail.com>
2024-01-17 21:27:47 +0100

    .

--- aquistobiglietti.php
+++ aquistobiglietti.php
@@ -2,9 +2,17 @@
 <html lang="en">
 	<head>			
 		<title>Acquisto biglietti - Zoom</title>
-		<meta charset="utf-8">
+		<link rel="stylesheet" href="./Css/acquistobiglietti.css">
 	</head>
 	<body>
 		<?php include './header.html';?>
+
+		<div class="container tipologia">
+			<h1>Biglietti</h1>
+			<h2 style="color: darkorange;">Tipologie di biglietto</h2>
+			<ul>
+				<li>ciao</li>
+			</ul>			
+		</div>
 	</body>
 </html>
