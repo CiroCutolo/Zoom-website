c301a12304ca6fa31be89216c98cedfa8e9e1a98
Gaetano Frasca <gaetanofra02@gmail.com>
2024-01-17 12:45:06 +0100

    modifiche db

--- connessione.php
+++ connessione.php
@@ -2,8 +2,8 @@
     $host = 'localhost';
     $port = '5432';
     $db = 'gruppo22';
-    $username = 'postgres';
-    $password = 'ciao'; //poi deve essere tw2024
+    $username = 'www';
+    $password = 'tw2024'; //poi deve essere tw2024
     $connection_string = "host = $host port = $port dbname = $db user = $username password = $password"; 
     //echo $connection_string;
 
