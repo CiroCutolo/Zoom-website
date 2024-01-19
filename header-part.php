5ed366b07c592a7e8c94e8532f795aaa064565b8
Claudiacarucci <claudia.carucci@hotmail.com>
2024-01-19 18:05:18 +0100

    orari e tariffe

--- header-part.php
+++ header-part.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <script src="https://kit.fontawesome.com/9491817803.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="header-part.css">
+    <link rel="stylesheet" href="header-part.css?<?php echo rand();?>">
     <title></title>
   </head>
   <body>
@@ -50,6 +50,9 @@
           <img class="photo" src="foto-tucano.png">
    </div>
    <div class="buy-tickets-container">
+    <!--Modifica dinamica dell'orario di ingresso tramite una funzione php che permette di ricavare la data corrente e di confrontarla con le
+    date di inizio e fine dell'orario estivo/invernale. Il confronto viene effettuato trasformando le date nel formato Unix timestamp in modo da
+    avere una quantità intera da poter inserire in una condizione if. La parte di tariffe resta statica.-->
       <?php 
       $today = date("d-m-Y");
       $current_year = date("Y");
@@ -57,17 +60,50 @@
       $winterend_time = "31-3-" . $current_year;
       $summerstart_time = "1-4-" . $current_year;
       $summerend_time = "31-10-" . $current_year;
-      if( strtotime($today) > strtotime($winterstart_time) && strtotime($today) < strtotime($winterend_time) ) {
-        echo "periodo invernale";
-      }else if ( strtotime($today) > strtotime($summerstart_time) && strtotime($today) < strtotime($summerend_time) ){
-        echo "peiodo estivo";
+      if( (strtotime($today) > strtotime($winterstart_time) && strtotime($today) < strtotime($winterend_time) ) || ( strtotime($today) == strtotime($winterstart_time) || strtotime($today) == strtotime($winterend_time))) {
+      ?>
+      <div class="hours">
+        <img src="foto-winter.png">
+        <h2>Orario Invernale</h2>
+        <div>Dal 1° Novembre al 31 Marzo: 9:30 - 17:30</div>
+        <div>Weekend e festivi invariati</div>
+      </div>
+      <?php 
+      }else if ( (strtotime($today) > strtotime($summerstart_time) && strtotime($today) < strtotime($summerend_time)) || ( strtotime($today) == strtotime($summerstart_time) || strtotime($today) == strtotime($summerend_time))){
+      ?>
+      <div class="hours">
+        <img src="foto-summer.png">
+        <h2>Orario Estivo</h2>
+        <div>Dal 1° Aprile al 31 Ottobre: 10:00 - 18:00</div>
+        <div>Weekend e festivi: 9:30 - 19:00</div>
+      </div>
+      <?php
       }else{
-        echo "nuovo periodo invernale";
+      ?>
+      <div class="hours">
+        <img src="foto-winter.png">
+        <h2>Orario Invernale</h2>
+        <div>Dal 1° Novembre al 31 Marzo: 9:30 - 17:30</div>
+        <div>Weekend e festivi invariati</div>
+      </div>
+      <?php
       }
       ?>
+      <div class="price">
+        <img src="foto-tickets.png">
+        <h2>Tariffe</h2>
+        <div>-Intero: 15€</div>
+        <div>-Ridotto: 10€ (4-10 anni, persone con disabilità inferiore al 100%)</div>
+        <div>-Gratuito: Under 3, over 70 e disabili 100% con accompagnatori</div>
+      </div>
+      <div class="price">
+        <div class="btn-tickets-container">
+          <button class="btn-tickets">LEGGI DI PIÙ</button>
+        </div>
+      </div>
    </div>
- </div>
-      <br>
+</div>
+<br>
  <div class="tag map">
       <div class="map-container">
         <div class="map-content">
