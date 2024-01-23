304eb5f20fa7bc4afa0382eccfa80a952a05bb9e
Gaetano Frasca <gaetanofra02@gmail.com>
2024-01-23 18:45:43 +0100

    fine biglietti

---
+++ JS/function.js
@@ -0,0 +1,8 @@
+function moreInfo(){
+	var x = document.getElementById("infoPanel");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
