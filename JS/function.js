0f98cd852f9426e1f6abff78e5c3209ec22f4c7d
Gaetano Frasca <gaetanofra02@gmail.com>
2024-02-01 12:18:47 +0100

    infopanel

--- JS/function.js
+++ JS/function.js
@@ -1,7 +1,8 @@
-function moreInfo(){
-	var x = document.getElementById("infoPanel");
-  if (x.style.display === "none") {
-    x.style.display = "block";
+function moreInfo(panelId){
+	var x = document.getElementById(panelId);
+  alert("ciao");
+  if (x.style.display == "none") {
+    x.style.display = "hidden";
   } else {
     x.style.display = "none";
   }
