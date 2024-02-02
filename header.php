7066778dc2a8a0cfde60dc9dd528bb991d42467a
Claudiacarucci <claudia.carucci@hotmail.com>
2024-02-02 10:30:45 +0100

    virgolette nella quary

--- header.php
+++ header.php
@@ -38,9 +38,9 @@
         </script>";
         }
       }
-    }else if(isset($_SESSION["isLogged"]) && $_SESSION["isLogged"]!="" ){
+    }else if(isset($_SESSION["isLogged"]) && $_SESSION["isLogged"]!=""){
       $em = $_SESSION["isLogged"];
-      $sql = "SELECT email,nome,cognome FROM utenti WHERE utenti.email=$em";
+      $sql = "SELECT email,nome,cognome FROM utenti WHERE utenti.email= '$em' ";
       $ret = pg_query($conn,$sql);
       $row = pg_fetch_row($ret);
       $_SESSION["email"] = $row[0];
